d07289937226e760635425570d60ee46de15560c
Niranjan Dukhande <progsolos@gmail.com>
2025-02-04 12:29:29 +0530

    week-18-next

---
+++ week-18/week-18-next/app/blogs/page.tsx
@@ -0,0 +1,31 @@
+import axios from "axios";
+async function getBlogs() {
+  const response = await axios.get(
+    "https://jsonplaceholder.typicode.com/todos/"
+  );
+  return response.data;
+}
+
+export default async function Blogs() {
+  const blogs = await getBlogs();
+  return (
+    <div>
+      {blogs.map((blog: TodoInterface) => (
+        <Todo title={blog.title} completed={blog.completed} />
+      ))}
+    </div>
+  );
+}
+
+interface TodoInterface {
+  title: string;
+  completed: boolean;
+}
+
+function Todo({ title, completed }: TodoInterface) {
+  return (
+    <div>
+      {title} {completed ? "done" : "not done"}
+    </div>
+  );
+}
---
+++ week-18/week-18-next/components/Navbar.tsx
@@ -0,0 +1,6 @@
+
+export function Navbar(){
+    return <div className="border-b p-4">
+        Quizzio
+    </div>
+}
